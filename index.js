b9cce4ddbe24a081ecae296e6655921b0f80eef8
Glitch (greet-bot) <none>
2025-05-09 07:10:59 +0000

    🍠🍉 Checkpoint ./package.json:78702115/26 ./index.js:78702115/1773

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // const { App } = require('@slack/bolt');
 // const { OpenAI } = require("openai");
 
-import { App } from "@slack/bolt";
-// import { OpenAI } from "openai";
+const { App } = require('@slack/bolt');
 
+// ボットトークンと Signing Secret を使ってアプリを初期化します
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
+
+(async () => {
+  // アプリを起動します
+  await app.start(process.env.PORT || 3000);
+
+  app.logger.info('⚡️ Bolt app is running!');
+})();
+
 
 // const openai = new OpenAI({
 //   apiKey: process.env.OPENAI_API_KEY,
@@ -98,8 +105,3 @@ const app = new App({
 //   return messageList;
 // };
 
-
-(async () => {
-  await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Bolt app is running!!');
-})();
